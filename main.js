83e4484cffa78db9a07c7a5329ece7df56bfddcd
JEFF HENRY <henry_jf@hotmail.com>
2016-03-02 13:53:41 -0500

    took comments out of main.js

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 //declare var;
 var num1, num2, button, answer;
+
 // elements;
-
-
 num1 = document.querySelector('#num_one');
 num2 = document.querySelector('#num_two');
 button = document.querySelector('#calculateBtn');
 answer = document.querySelector('#answer');
+ 
  // functions 1 for math ; 
-
 function add() {
  	// console.log(Number(num1.value) + Number(num2.value));
  	return (Number(num1.value) + Number(num2.value));
  }
 
- 
  // funtion 2 show calculation;
 function print() {
 	answer.textContent = add();
